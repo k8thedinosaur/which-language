2062c685e2003568a98bb0afabaca1d3c38b0eaa
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-27 10:05:36 -0700

    use console.log to check value of selected radio button

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,15 @@ var ruby = 0;
 var c = 0;
 var js = 0;
 
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     
+    var question1 = $("input:radio[name=question1]:checked").val();
+    console.log(question1);
+
+       
+
   })
 })
