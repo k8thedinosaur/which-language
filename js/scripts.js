ab4632d93b52678a8872963e14c83252ead893b7
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-27 12:56:04 -0700

    add span background colors to answers for ease of testing

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
       c +=1;
     } else if (question1 === "js") {
       js +=1;
-    }
+    };
 
     var question2 = $("input:radio[name=question2]:checked").val();
     if (question2 === "python") {
@@ -28,7 +28,7 @@ $(document).ready(function() {
       c +=1;
     } else if (question2 === "js") {
       js +=1;
-    }
+    };
 
     var question3 = $("input:radio[name=question2]:checked").val();
     if (question3 === "python") {
@@ -39,84 +39,84 @@ $(document).ready(function() {
       c +=1;
     } else if (question3 === "js") {
       js +=1;
-    }
+    };
 
-    var question4 = $("input:radio[name=question2]:checked").val();
-    if (question4 === "python") {
-      python +=1;
-    } else if (question4 === "ruby") {
-      ruby +=1;
-    } else if (question4 === "c") {
-      c +=1;
-    } else if (question4 === "js") {
-      js +=1;
-    }
+    // var question4 = $("input:radio[name=question2]:checked").val();
+    // if (question4 === "python") {
+    //   python +=1;
+    // } else if (question4 === "ruby") {
+    //   ruby +=1;
+    // } else if (question4 === "c") {
+    //   c +=1;
+    // } else if (question4 === "js") {
+    //   js +=1;
+    // };
 
-    var question5 = $("input:radio[name=question2]:checked").val();
-    if (question5 === "python") {
-      python +=1;
-    } else if (question5 === "ruby") {
-      ruby +=1;
-    } else if (question5 === "c") {
-      c +=1;
-    } else if (question5 === "js") {
-      js +=1;
-    }
+    // var question5 = $("input:radio[name=question2]:checked").val();
+    // if (question5 === "python") {
+    //   python +=1;
+    // } else if (question5 === "ruby") {
+    //   ruby +=1;
+    // } else if (question5 === "c") {
+    //   c +=1;
+    // } else if (question5 === "js") {
+    //   js +=1;
+    // };
 
-    var question6 = $("input:radio[name=question2]:checked").val();
-    if (question6 === "python") {
-      python +=1;
-    } else if (question6 === "ruby") {
-      ruby +=1;
-    } else if (question6 === "c") {
-      c +=1;
-    } else if (question6 === "js") {
-      js +=1;
-    }
+    // var question6 = $("input:radio[name=question2]:checked").val();
+    // if (question6 === "python") {
+    //   python +=1;
+    // } else if (question6 === "ruby") {
+    //   ruby +=1;
+    // } else if (question6 === "c") {
+    //   c +=1;
+    // } else if (question6 === "js") {
+    //   js +=1;
+    // };
 
-    var question7 = $("input:radio[name=question2]:checked").val();
-    if (question7 === "python") {
-      python +=1;
-    } else if (question7 === "ruby") {
-      ruby +=1;
-    } else if (question7 === "c") {
-      c +=1;
-    } else if (question7 === "js") {
-      js +=1;
-    }
+    // var question7 = $("input:radio[name=question2]:checked").val();
+    // if (question7 === "python") {
+    //   python +=1;
+    // } else if (question7 === "ruby") {
+    //   ruby +=1;
+    // } else if (question7 === "c") {
+    //   c +=1;
+    // } else if (question7 === "js") {
+    //   js +=1;
+    // };
 
-    var question8 = $("input:radio[name=question2]:checked").val();
-    if (question8 === "python") {
-      python +=1;
-    } else if (question8 === "ruby") {
-      ruby +=1;
-    } else if (question8 === "c") {
-      c +=1;
-    } else if (question8 === "js") {
-      js +=1;
-    }
+    // var question8 = $("input:radio[name=question2]:checked").val();
+    // if (question8 === "python") {
+    //   python +=1;
+    // } else if (question8 === "ruby") {
+    //   ruby +=1;
+    // } else if (question8 === "c") {
+    //   c +=1;
+    // } else if (question8 === "js") {
+    //   js +=1;
+    // };
 
-    var question9 = $("input:radio[name=question2]:checked").val();
-    if (question9 === "python") {
-      python +=1;
-    } else if (question9 === "ruby") {
-      ruby +=1;
-    } else if (question9 === "c") {
-      c +=1;
-    } else if (question9 === "js") {
-      js +=1;
-    }
+    // var question9 = $("input:radio[name=question2]:checked").val();
+    // if (question9 === "python") {
+    //   python +=1;
+    // } else if (question9 === "ruby") {
+    //   ruby +=1;
+    // } else if (question9 === "c") {
+    //   c +=1;
+    // } else if (question9 === "js") {
+    //   js +=1;
+    // };
 
-    var question10 = $("input:radio[name=question2]:checked").val();
-    if (question10 === "python") {
-      python +=1;
-    } else if (question10 === "ruby") {
-      ruby +=1;
-    } else if (question10 === "c") {
-      c +=1;
-    } else if (question10 === "js") {
-      js +=1;
-    }
+    // var question10 = $("input:radio[name=question2]:checked").val();
+    // if (question10 === "python") {
+    //   python +=1;
+    // } else if (question10 === "ruby") {
+    //   ruby +=1;
+    // } else if (question10 === "c") {
+    //   c +=1;
+    // } else if (question10 === "js") {
+    //   js +=1;
+    // };
 
     console.log("python: " + python)
     console.log("ruby: " + ruby)
