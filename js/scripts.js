15c2445d214f20f6c9e70e15ee661d8e9dded541
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-27 12:39:40 -0700

    add jq logic which worked on 3 questions to 10 questions and break all the things!

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
     event.preventDefault();
     
     var question1 = $("input:radio[name=question1]:checked").val();
-    console.log(question1);
     if (question1 === "python") {
       python +=1;
     } else if (question1 === "ruby") {
@@ -21,7 +20,6 @@ $(document).ready(function() {
     }
 
     var question2 = $("input:radio[name=question2]:checked").val();
-    console.log(question1);
     if (question2 === "python") {
       python +=1;
     } else if (question2 === "ruby") {
@@ -29,6 +27,94 @@ $(document).ready(function() {
     } else if (question2 === "c") {
       c +=1;
     } else if (question2 === "js") {
+      js +=1;
+    }
+
+    var question3 = $("input:radio[name=question2]:checked").val();
+    if (question3 === "python") {
+      python +=1;
+    } else if (question3 === "ruby") {
+      ruby +=1;
+    } else if (question3 === "c") {
+      c +=1;
+    } else if (question3 === "js") {
+      js +=1;
+    }
+
+    var question4 = $("input:radio[name=question2]:checked").val();
+    if (question4 === "python") {
+      python +=1;
+    } else if (question4 === "ruby") {
+      ruby +=1;
+    } else if (question4 === "c") {
+      c +=1;
+    } else if (question4 === "js") {
+      js +=1;
+    }
+
+    var question5 = $("input:radio[name=question2]:checked").val();
+    if (question5 === "python") {
+      python +=1;
+    } else if (question5 === "ruby") {
+      ruby +=1;
+    } else if (question5 === "c") {
+      c +=1;
+    } else if (question5 === "js") {
+      js +=1;
+    }
+
+    var question6 = $("input:radio[name=question2]:checked").val();
+    if (question6 === "python") {
+      python +=1;
+    } else if (question6 === "ruby") {
+      ruby +=1;
+    } else if (question6 === "c") {
+      c +=1;
+    } else if (question6 === "js") {
+      js +=1;
+    }
+
+    var question7 = $("input:radio[name=question2]:checked").val();
+    if (question7 === "python") {
+      python +=1;
+    } else if (question7 === "ruby") {
+      ruby +=1;
+    } else if (question7 === "c") {
+      c +=1;
+    } else if (question7 === "js") {
+      js +=1;
+    }
+
+    var question8 = $("input:radio[name=question2]:checked").val();
+    if (question8 === "python") {
+      python +=1;
+    } else if (question8 === "ruby") {
+      ruby +=1;
+    } else if (question8 === "c") {
+      c +=1;
+    } else if (question8 === "js") {
+      js +=1;
+    }
+
+    var question9 = $("input:radio[name=question2]:checked").val();
+    if (question9 === "python") {
+      python +=1;
+    } else if (question9 === "ruby") {
+      ruby +=1;
+    } else if (question9 === "c") {
+      c +=1;
+    } else if (question9 === "js") {
+      js +=1;
+    }
+
+    var question10 = $("input:radio[name=question2]:checked").val();
+    if (question10 === "python") {
+      python +=1;
+    } else if (question10 === "ruby") {
+      ruby +=1;
+    } else if (question10 === "c") {
+      c +=1;
+    } else if (question10 === "js") {
       js +=1;
     }
 
