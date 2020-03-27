68aa9f0d8e12225a2b434cbbc44558eef5f2160a
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-27 10:30:18 -0700

    add second question and console.log values

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,19 @@ $(document).ready(function() {
     } else if (question1 === "js") {
       js +=1;
     }
+
+    var question2 = $("input:radio[name=question2]:checked").val();
+    console.log(question1);
+    if (question2 === "python") {
+      python +=1;
+    } else if (question2 === "ruby") {
+      ruby +=1;
+    } else if (question2 === "c") {
+      c +=1;
+    } else if (question2 === "js") {
+      js +=1;
+    }
+
     console.log("python: " + python)
     console.log("ruby: " + ruby)
     console.log("c: " + c)
